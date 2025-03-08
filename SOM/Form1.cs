c21229d6f7ac0d5c889a341a4b033a351b25e829
Simay Gocen <simaygocen@gmail.com>
2025-03-08 17:23:52 +0300

    Proje dosyası ekle.

---
+++ SOM/Form1.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SOM
+{
+    public partial class Form1 : Form
+    {
+        private Button[] clusterButtons; 
+        public Map map; 
+
+        public Form1()
+        {
+            InitializeComponent();
+             
+        }
+
+        private void ClusterButton_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+
+            // Taking cluster coordinates from button
+            string[] coordinates = button.Text.Split(',');
+            int x = int.Parse(coordinates[0]);
+            int y = int.Parse(coordinates[1]);
+
+            // Taking text representations of all instances belonging to the relevant cluster coordinates
+            string clusterExamples = GetClusterExamples(x, y);
+
+            MessageBox.Show(clusterExamples, "Instances in "+x+","+y, MessageBoxButtons.OK);
+        }
+
+        private string GetClusterExamples(int x, int y)
+        {
+            StringBuilder builder = new StringBuilder();
+
+            string filePath = textBox1.Text;
+
+            if (File.Exists(filePath))
+            {
+                
+                string[] lines = File.ReadAllLines(filePath);
+
+                string featuresLine = lines[0];
+
+                builder.AppendLine(featuresLine);
+
+                // Creating a Dictionary to store instances
+                Dictionary<int, string> instances = new Dictionary<int, string>();
+
+                // Adding lines other than the features line
+                for (int i = 1; i < lines.Length; i++)
+                {
+                    instances.Add(i - 1, lines[i]); 
+                }
+
+                // Finding the ones belonging to the relevant cluster by going through all the instances
+                for (int i = 0; i < map.patterns.Count; i++)
+                {
+                    // If the instance belongs to the relevant cluster
+                    if (map.outputs[x, y] == map.Winner(map.patterns[i]))
+                    {
+                        //Console.WriteLine(i);
+                        string exampleText = instances[i];
+                        builder.AppendLine(exampleText);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("File Not Found!");
+            }
+            return builder.ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            {
+                openFileDialog1.ShowDialog();
+                if (!string.IsNullOrEmpty(openFileDialog1.FileName))
+                {
+                    string fileName = Path.GetFileName(openFileDialog1.FileName);
+                    textBox1.Text = fileName;
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            int dimension = 0;
+            if (int.TryParse(textBox2.Text, out dimension))
+            {
+                if (!string.IsNullOrEmpty(textBox1.Text) && File.Exists(textBox1.Text))
+                {
+                    StreamReader sr = new StreamReader(textBox1.Text);
+                    int lng = sr.ReadLine().Split(',').Length;
+
+                    map = new Map(lng-1, dimension,textBox1.Text);
+                    InitializeClusterButtons();
+                }
+                else
+                {
+                    MessageBox.Show("Please select a valid file.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Dimension is invalid.");
+            }
+        }
+        private void InitializeClusterButtons()
+        {
+            TableLayoutPanel tableLayoutPanel = new TableLayoutPanel();
+            tableLayoutPanel.ColumnStyles.Clear();
+            tableLayoutPanel.RowStyles.Clear();
+
+            int rows = map.length;
+            int cols = map.length;
+
+            for (int i = 0; i < rows; i++)
+            {
+                tableLayoutPanel.RowStyles.Add(new RowStyle(SizeType.Percent, 100f / rows));
+
+                for (int j = 0; j < cols; j++)
+                {
+                    tableLayoutPanel.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 100f / cols));
+
+                    int index = i * cols + j;
+                    Button button = new Button();
+                    button.Text = $"{i},{j}";
+                    button.Tag = index;
+                    button.Click += ClusterButton_Click;
+                    button.BackColor = Color.Orange; 
+
+                    tableLayoutPanel.Controls.Add(button, j, i);
+                }
+            }
+
+            tableLayoutPanel.Size = new Size(600, 400);
+            tableLayoutPanel.Location = new Point(10, 80);
+            tableLayoutPanel.Dock = DockStyle.None; 
+            Controls.Add(tableLayoutPanel);
+        }
+    }
+}
---
+++ SOM/Program.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Windows.Forms;
+
+namespace SOM
+{
+
+    public class Map
+    {
+        public Neuron[,] outputs;  // Collection of weights.
+        public int iteration;      // Current iteration.
+        public int length;        // Side length of output grid.
+        public int dimensions;    // Number of input dimensions.
+        public Random rnd = new Random();
+
+        public List<string> labels = new List<string>();
+        public List<double[]> patterns = new List<double[]>();
+
+        public List<string> patterns_with_target_attr = new List<string>();
+
+        [STAThread]
+        static void Main(string[] args)
+        {    
+            Application.Run(new Form1());
+        }
+
+        public Map(int dimensions, int length, string file)
+        {
+            this.length = length;
+            this.dimensions = dimensions;
+            Initialise();
+            LoadData(file);
+            NormalisePatterns();
+            Train(0.0000001);
+            DumpCoordinates();
+
+            double sse = 0;
+            for(int i=0;i<outputs.GetLength(0);i++)
+            {
+                for (int j = 0; j < outputs.GetLength(1); j++)
+                {
+                    sse += outputs[i, j].sse1();
+                }
+            }
+
+            Console.WriteLine(sse);
+            //Console.ReadLine();
+        }
+
+        private void Initialise()
+        {
+            outputs = new Neuron[length, length];
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    outputs[i, j] = new Neuron(i, j, length);
+                    outputs[i, j].Weights = new double[dimensions];
+                    outputs[i, j].total_attr_values = new double[dimensions];
+                    for (int k = 0; k < dimensions; k++)
+                    {
+                        outputs[i, j].Weights[k] = rnd.NextDouble();
+                    }
+                }
+            }
+        }
+
+        private void LoadData(string file)
+        {
+            StreamReader reader = File.OpenText(file);
+            reader.ReadLine(); // Ignore first line.
+            while (!reader.EndOfStream)
+            {
+                string[] line = reader.ReadLine().Split(',');
+                labels.Add(line[0]);
+                double[] inputs = new double[dimensions];
+                for (int i = 0; i < dimensions; i++)
+                {
+                    inputs[i] = double.Parse(line[i]);
+
+                }
+                patterns.Add(inputs);
+                patterns_with_target_attr.Add(line[dimensions]);
+            }
+            reader.Close();
+        }
+
+        private void NormalisePatterns()
+        {
+            for (int j = 0; j < dimensions; j++)
+            {
+                //double sum = 0;
+                double max = 0;
+
+                for (int i = 0; i < patterns.Count; i++)
+                {
+                    if (patterns[i][j] > max) max = patterns[i][j];
+                    //sum += patterns[i][j];
+                }
+                //double average = sum / patterns.Count;
+                for (int i = 0; i < patterns.Count; i++)
+                {
+                    //patterns[i][j] = patterns[i][j] / average;
+                    patterns[i][j] = patterns[i][j] / max;
+                }
+            }
+        }
+
+        private void Train(double maxError)
+        {
+            double currentError = double.MaxValue;
+            while (currentError > maxError)
+            {
+                currentError = 0;
+                List<double[]> TrainingSet = new List<double[]>();
+                foreach (double[] pattern in patterns)
+                {
+                    TrainingSet.Add(pattern);
+                }
+                for (int i = 0; i < patterns.Count; i++)
+                {
+                    double[] pattern = TrainingSet[rnd.Next(patterns.Count - i)];
+                    currentError += TrainPattern(pattern);
+                    TrainingSet.Remove(pattern);
+                }
+                Console.WriteLine(currentError.ToString("0.0000000"));
+            }
+        }
+
+        public double TrainPattern(double[] pattern)
+        {
+            double error = 0;
+            Neuron winner = Winner(pattern);
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j < length; j++)
+                {
+                    error += outputs[i, j].UpdateWeights(pattern, winner, iteration);
+                }
+            }
+            iteration++;
+            return Math.Abs(error / (length * length));
+        }
+
+        public void DumpCoordinates()
+        {
+           
+            for (int i = 0; i < patterns.Count; i++)
+            {
+                Neuron n = Winner(patterns[i]);
+                n.data.Add(patterns[i]);
+                n.tr.Add(patterns_with_target_attr[i]);
+
+                for (int j = 0; j < patterns[i].Length; j++)
+                    n.total_attr_values[j] += patterns[i][j];
+
+                Console.WriteLine("{0},{1},{2}", i, n.X, n.Y);
+            }
+
+            
+        }
+
+        public Neuron Winner(double[] pattern)
+        {
+            Neuron winner = null;
+            double min = double.MaxValue;
+            for (int i = 0; i < length; i++)
+                for (int j = 0; j < length; j++)
+                {
+                    double d = Distance(pattern, outputs[i, j].Weights);
+                    if (d < min)
+                    {
+                        min = d;
+                        winner = outputs[i, j];
+                    }
+                }
+            return winner;
+        }
+
+        private double Distance(double[] vector1, double[] vector2)
+        {
+            double value = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                value += Math.Pow((vector1[i] - vector2[i]), 2);
+            }
+            return Math.Sqrt(value);
+        }
+    }
+
+    public class Neuron
+    {
+        public double[] Weights;
+        public int X;
+        public int Y;
+        private int length;
+        private double nf;
+        public List<double[]> data;
+        public List<string> tr;
+        
+
+        public double sse1()
+        {
+            double retV = 0;
+            for(int i=0;i<data.Count;i++)
+            {
+                retV += Distance(Weights, data[i]);
+            }
+            return retV;
+        }
+
+        private double Distance(double[] vector1, double[] vector2)
+        {
+            double value = 0;
+            for (int i = 0; i < vector1.Length; i++)
+            {
+                value += Math.Pow((vector1[i] - vector2[i]), 2);
+            }
+            return value;
+        }
+
+
+        public double[] total_attr_values;
+
+
+
+        public Neuron(int x, int y, int length)
+        {
+            X = x;
+            Y = y;
+            this.length = length;
+            nf = 1000 / Math.Log(length);
+            data = new List<double[]>();
+            tr = new List<string>();
+        }
+
+        private double Gauss(Neuron win, int it)
+        {
+            double distance = Math.Sqrt(Math.Pow(win.X - X, 2) + Math.Pow(win.Y - Y, 2));
+            return Math.Exp(-Math.Pow(distance, 2) / (Math.Pow(Strength(it), 2)));
+        }
+
+        private double LearningRate(int it)
+        {
+            return Math.Exp(-it / 1000) * 0.1;
+        }
+
+        private double Strength(int it)
+        {
+            return Math.Exp(-it / nf) * length;
+        }
+
+        public double UpdateWeights(double[] pattern, Neuron winner, int it)
+        {
+            double sum = 0;
+            for (int i = 0; i < Weights.Length; i++)
+            {
+                double delta = LearningRate(it) * Gauss(winner, it) * (pattern[i] - Weights[i]);
+                Weights[i] += delta;
+                sum += delta;
+            }
+            return sum / Weights.Length;
+        }
+    }
+}
